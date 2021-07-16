7ca0b5151d23689c6b8f8da4ddc4130d255c73e7
Boris Sobolev <rafinad@outlook.com>
2021-07-16 22:33:35 +0300

    Initial Commit

---
+++ ButtonsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonsTest
+//
+//  Created by Boris Sobolev on 16.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
